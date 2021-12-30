9c8bb2f3026335793bd736285489dcacb7e9808b
ksheng <ksheng@enchoice.com>
2021-12-30 13:10:34 -0500

    after test container

---
+++ cmds/build-container.sh
@@ -0,0 +1,8 @@
+docker build -f Dockerfile.dev .
+
+docker run -p 3000:3000 -v $(pwd):/app kevinjsheng/frontend
+
+docker run -p 3000:3000 -v /app/mode_modules -v $(pwd):/app kevinjsheng/frontend
+
+# run npm run test in the container
+docker run -it kevinjsheng/frontend npm run test
